d4945d15d2e285b526dbb5a193a6082d2d91e7ef
Mayuresh Waykole <mayuresh2212@gmail.com>
2018-01-09 03:07:06 -0500

    sails.js inital commit

--- app/models/user.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const Appuser = sequelize.define('appuser', {
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    password: DataTypes.STRING,
-  });
-  return Appuser;
-};
